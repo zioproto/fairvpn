4005d7ed3202db492addd4fada2b4de9a1a0f954
Saverio Proto <zioproto@gmail.com>
2010-02-05 15:54:31 +0100

    First commit, parsing OLSR topology and have hand ready nodes and links

---
+++ fairvpn/fairvpn.py
@@ -0,0 +1,56 @@
+#! /usr/bin/env python
+import os
+import re
+
+# Authors: Saverio Proto
+# This software is released under GPL3
+# Copyright Ninux.org 2010
+
+#This script reads the the txt file from olsrd_txtinfo plug-in and ...
+#Needs wget packages
+
+bootstrap = "127.0.0.1"
+
+########################### IMPLEMENTATION #############################
+
+#download topology
+#os.system(" wget http://"+bootstrap+":2006 -q -O topology.txt")
+
+#open file
+topology_file=open("topology.txt",'r')
+parsing=False
+
+nodedb = set()
+links = {}
+
+for line in topology_file.readlines():
+	if parsing:
+		if line.isspace():
+			parsing=False
+			print "List of collected nodes:"
+			for node in nodedb:
+				print node
+			print "list of collected links:"
+			for link,etx in links.iteritems():
+				print link + "\t",etx
+			break
+		endpoint1=line.split()[0] #IP address of endpoint1
+		endpoint2=line.split()[1] #IP address of endpoint2
+		
+		try:
+			etx=float(line.split()[4])
+		except:
+			etx = 999999
+		
+		if endpoint1 not in nodedb:
+                                nodedb.add(endpoint1) 
+		if endpoint2 not in nodedb:
+                                nodedb.add(endpoint2) 
+		if endpoint1 + '\t' + endpoint2 not in links:
+				links[endpoint1 + '\t' +endpoint2] = etx
+	
+		#print "searching ip: %s and the other ip %s and cost %s" % (endpoint1,endpoint2,etx) 	
+				
+	if line.find('Dest. IP') != -1:
+		parsing=True	
+
