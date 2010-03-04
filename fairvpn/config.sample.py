c41af2d3efe41d42b05dedb9e40f8de9ded28b7a
Saverio Proto <zioproto@gmail.com>
2010-03-04 17:40:16 +0100

    Adding sample config

---
+++ fairvpn/config.sample.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+
+tinc_cmd = "tincd --config=./ --bypass-security -d2 -D"
+myOverlayIP = ""
+#myOverlayIP = "10.%d.%d.%d" % (random.randint(1,254),random.randint(1,254),random.randint(1,254))
+bootstrap = "160.80.81.106"
+bootstrapName = "x10x0x30x1"
+fanout = 2
