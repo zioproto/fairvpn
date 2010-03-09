943c47cc9fb6e305bcb94dc127ac5e1f45e6a972
Saverio Proto <zioproto@gmail.com>
2010-03-09 17:11:37 +0100

    Adding prototype of netkit lab

---
+++ netkitlab/node3/config.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+
+tinc_cmd = "tincd --config=./ --bypass-security -d2 -D"
+myOverlayIP = "10.0.0.3"
+#myOverlayIP = "10.%d.%d.%d" % (random.randint(1,254),random.randint(1,254),random.randint(1,254))
+bootstrap = "160.80.80.254"
+bootstrapName = "x10x0x0x254"
+fanout = 2
