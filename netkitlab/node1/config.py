2f603daf9343a88ff752c5d942eb755328e4e4ea
claudio <claudio@claudio-laptop>
2010-05-05 12:42:53 +0200

    ip overlay nodi configurato in modo random

--- netkitlab/node1/config.py
+++ netkitlab/node1/config.py
@@ -1,8 +1,9 @@
 #! /usr/bin/env python
+import random
 
 tinc_cmd = "./tincd --config=./ --bypass-security"
-myOverlayIP = "10.0.0.1"
-#myOverlayIP = "10.%d.%d.%d" % (random.randint(1,254),random.randint(1,254),random.randint(1,254))
+#myOverlayIP = "10.0.0.1"
+myOverlayIP = "10.%d.%d.%d" % (random.randint(1,254),random.randint(1,254),random.randint(1,254))
 bootstrap = "160.80.0.254"
 bootstrapName = "bootnode"
 fanout = 2
