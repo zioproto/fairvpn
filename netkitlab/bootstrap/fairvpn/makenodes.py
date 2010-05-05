a8d610c655328c2822d69caca7a16e88fb7128d2
claudio <claudio@claudio-laptop>
2010-05-05 12:51:50 +0200

    Aggiunto file che presa una lista di nodi di una slice, crea la cartella hosts di tinc e genera per ogni host un file con il campo address ed il rispettivo IP pubblico

---
+++ netkitlab/bootstrap/fairvpn/makenodes.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import socket
+from socket import gethostbyname
+
+def nodeonplanetlab():	
+	os.system("wget \"http://comon.cs.princeton.edu/status/tabulator.cgi?table=table_nodeviewshort&format=nameonly&persite=1&select='resptime>0\" -O ./hosts/planetnode.txt")
+
+	print "I'm downloading the list of nodes and resolve IP addresses"
+
+	x="./hosts/planetnode.txt"
+	f=open(x,"r")
+	allfile=f.readlines()
+	f.seek(0,0)
+	for i in range (len(allfile)):
+		linea=f.readline()
+		pathf="./hosts/"+linea[0:len(linea)-1];
+		newf=open(pathf,"w")		
+		xx="Address = "+socket.gethostbyname(linea[0:len(linea)-1])
+		newf.write(xx);
+		newf.close()
+
+	f.close()
+
+#MAIN
+
+nodeonplanetlab()
+os.popen("./start.sh")
+
+
