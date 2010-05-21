ca233be16db5c4ec4a3201e40429377c62465b85
Saverio Proto <zioproto@gmail.com>
2010-05-21 18:15:17 +0200

    Improved the script to distinguish between failed authentication and timeout

--- planet-lab/CheckSsh.py
+++ planet-lab/CheckSsh.py
@@ -14,27 +14,39 @@ else:
 
 	hosts = []
 	goodhosts=[]
+	noauth = 0
+	timeouts = 0
 	
 	hosts = [line.strip() for line in open("./nodes.txt")]
 
 	print hosts
 	print "start"
 	for host in hosts:
-		print "connecting to %s" % host
+		#print "connecting to %s" % host
 		try:	
 			s=socket.socket()
-			s.settimeout(1)
+			s.settimeout(3)
 			s.connect((host, 22))
 			t = paramiko.Transport(s)
 			t.connect(username=username,hostkey=None,password=None,pkey=mykey)
-			print t.is_authenticated()
+			#print t.is_authenticated()
 			if (t.is_authenticated()): 
 				goodhosts.append(host)
 			del t
-		except Exception:
-			print "Not Good"
+		except paramiko.AuthenticationException:
+			noauth = noauth + 1
+			print "connecting to %s" % host
+			print "Authentication Exception %d" % noauth
+
+		except socket.timeout:
+			timeouts = timeouts + 1
+			print "connecting to %s" % host
+			print "Timeouts! %d" % timeouts
 
 	print goodhosts
+	
+	print "\nAuthentication errors: %d\n" % noauth
+	print "\nTimeout errors %d\n" % timeouts
 
 filegood = open("./good.txt","w")
 
