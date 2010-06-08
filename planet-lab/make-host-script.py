4aa298d832f2eb19ae5eb6e323761475cb8b8761
Claudio Marini <claudio.marini.mail@gmail.com>
2010-06-08 12:16:33 +0200

    This script make the necessary script used by tincd

---
+++ planet-lab/make-host-script.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import socket
+#this script make the host script for tincd
+
+#MAIN#
+if( len(sys.argv)<2):
+	print "bad command: type python %s <list_of_nodes_in_use>" % sys.argv[0]
+	sys.exit(1);
+else:
+	lista=open(sys.argv[1],"r")
+	for linea in lista.readlines():
+		# Host-down Script
+		nomefile="./hosts/"+linea.strip()+"-down"
+		print nomefile
+		f=open(nomefile,"w")
+		f.write("#!/usr/bin/python \npython recover.py "+linea.strip()+" "+sys.argv[1]);
+		f.close()
+
+		# Host script
+		pathf="./hosts/"+linea.strip();
+                newf=open(pathf,"w")
+                xx="Address = "+socket.gethostbyname(linea.strip())
+                newf.write(xx);
+                newf.close()
+
+	lista.close()
+
+		
+
+
