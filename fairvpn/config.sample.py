139ebeb75910ab830a314711feade1c5f0aee027
Saverio Proto <zioproto@gmail.com>
2010-10-06 16:50:44 +0200

    Update default values

--- fairvpn/config.sample.py
+++ fairvpn/config.sample.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python
 
-tinc_cmd = "./tincd --config=./ --bypass-security -d2 -D"
+tinc_cmd = "/fairvpn/bin/tincd --config=./ --bypass-security -d2 -D"
 myOverlayIP = ""
 #myOverlayIP = "10.%d.%d.%d" % (random.randint(1,254),random.randint(1,254),random.randint(1,254))
-bootstrap = "160.80.81.106"
-bootstrapName = "x10x0x30x1"
+bootstrap = "160.80.103.148"
+bootstrapName = "bootnode"
 fanout = 2
