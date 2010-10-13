da20b54099773493b0a85067ce7643a8a139bd04
Saverio Proto <zioproto@gmail.com>
2010-10-13 15:22:19 +0200

    Getting rid of the problem with the pid file

--- fairvpn/config.sample.py
+++ fairvpn/config.sample.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-tinc_cmd = "/fairvpn/bin/tincd --config=./ --bypass-security -d2 -D"
+tinc_cmd = "/fairvpn/bin/tincd --pidfile=/tmp/fairvpn.pid --config=./ --bypass-security -d2 -D"
 bootstrap = "160.80.103.148"
 bootstrapName = "bootnode"
 fanout = 2
