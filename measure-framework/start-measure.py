2706b9b9bddfa120bd852320ebfa1506618023f9
Saverio Proto <zioproto@gmail.com>
2010-10-13 18:59:15 +0200

    Tested threads

--- measure-framework/start-measure.py
+++ measure-framework/start-measure.py
@@ -3,6 +3,7 @@ import sys
 import socket
 import random
 from threading import Thread
+#import time
 
 class schiavo(Thread):
 	def __init__ (self,ips,ipd):
@@ -11,6 +12,7 @@ class schiavo(Thread):
 		self.ipd = ipd
 		self.status = -1
 	def run(self):
+		#time.sleep(2)
 		cmd = "ossh -l root "+self.ips+" \"iperf -c "+self.ipd+" -y C\" > outputmisure"
 		print cmd
 		#os.system(cmd)
@@ -48,3 +50,4 @@ for nodo in nodes:
 	current = schiavo(nodo,random.choice(nodes))	
 	current.start()
 
+print "All threads launched"
