321c076d365d4c60044a6774488fb47afeb1ada6
Saverio Proto <zioproto@gmail.com>
2010-10-15 17:37:22 +0200

    big changes testing on field the script

--- measure-framework/start-measure.py
+++ measure-framework/start-measure.py
@@ -14,16 +14,20 @@ def generatenumber():
 
 
 class schiavo(Thread):
-	def __init__ (self,ips,ipd):
+	def __init__ (self,ips):
 		Thread.__init__(self)
 		self.ips = ips
-		self.ipd = ipd
 		self.status = -1
 	def run(self):
 		#time.sleep(2)
 		self.alive = True
 		while(self.alive):
+			self.ipd = random.choice(nodes_overlay)
+			while (self.ipd == self.ips):
+				self.ipd = random.choice(nodes_overlay)
 			numK=int(generatepareto());
+			if (numK > 5):
+				 numK = 5
 			cmd = ossh+"  -l root "+self.ips+" \"iperf -c "+self.ipd+" -n "+str(numK)+"M -y C\" >> outputmisure"
 			#print cmd
 			print "Start thread, source:\t" +self.ips+"\tand destination:\t"+ self.ipd+"\tBytes: "+str(numK)+"M\n"
@@ -35,7 +39,7 @@ class schiavo(Thread):
 
 #MAIN#
 
-ossh="ssh -o \"UserKnownHostsFile /dev/null\" -o \"StrictHostKeyChecking no\""
+ossh="ssh -o \"UserKnownHostsFile /dev/null\" -o \"StrictHostKeyChecking no\" -o \"TCPKeepAlive yes\""
 
 nodes_overlay = []
 nodes_underlay = []
@@ -64,16 +68,13 @@ lista.close()
 threads_list = []
 
 for nodo in nodes_overlay:
-	dest = random.choice(nodes_overlay)
-	while (dest == nodo):
-		dest = random.choice(nodes_overlay)
-	current = schiavo(nodo,dest)
+	current = schiavo(nodo)
 	threads_list.append(current)
 	current.start()
 
 print "All threads launched"
 
-for i in range (1,120):
+for i in range (1,1800):
 	print "Seconds:"+str(i)+"\n"
 	time.sleep(1)
 
