03fa3e55a574799b19f03d238ae9be05cc3aaac5
Saverio Proto <zioproto@gmail.com>
2010-11-11 12:16:15 +0100

    Adding more nodes to Netkit lab

--- netkitlab/node4/config.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python
-import random
-
-tinc_cmd = "./tincd --config=./ --bypass-security "
-#myOverlayIP = "10.0.0.4"
-myOverlayIP = "10.%d.%d.%d" % (random.randint(1,254),random.randint(1,254),random.randint(1,254))
-bootstrap = "160.80.0.254"
-bootstrapName = "bootnode"
-fanout = 2
