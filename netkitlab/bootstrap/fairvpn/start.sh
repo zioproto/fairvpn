11f910dccca508f686a2b8569cf6263e5939d158
Saverio Proto <zioproto@gmail.com>
2010-11-11 12:40:07 +0100

    Adding more nodes

--- netkitlab/bootstrap/fairvpn/start.sh
+++ netkitlab/bootstrap/fairvpn/start.sh
@@ -1,6 +1,10 @@
-rsync --daemon 
-tincd --bypass-security --config=. -K -d2 -D<<EOF
+rm /fairvpn/hosts/nodes
+touch /fairvpn/hosts/nodes
+/fairvpn/bin/rsync --daemon --config=/fairvpn/rsyncd.conf
+/fairvpn/bin/olsrd -f /fairvpn/olsrd.conf
+/fairvpn/bin/tincd --bypass-security --config=. -K -d2 -D<<EOF
 
 
 EOF
-tincd --bypass-security --config=. -d2 -D
+/fairvpn/bin/tincd --bypass-security --config=. -d2 -D
+
